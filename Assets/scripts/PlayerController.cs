fa572c9dfe1321d7eced109884314b6d92d62943
nevanlittle <nevan.little@hotmail.com>
2026-01-28 15:50:00 -0700

    Character no longer has friction with the ground and made the play space larger

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -7,12 +7,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpForce = 20f;
 
     [SerializeField] private InputManager inputManager;
+    private bool _canJump => _isGrounded || Time.time < _lastGroundedCheck + _coyoteTime;
+    [SerializeField] private float _lastGroundedCheck;
+    [SerializeField] private float _coyoteTime = 0.3f;
     [Header("Ground Check")] 
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Vector2 startPointOffSet;
     [SerializeField] private float groundCheckDistance;
     private float _horizontalInput = 0;
     private bool _isGrounded = false;
+    
     
     void Awake()
     {
@@ -40,7 +44,7 @@ public class PlayerController : MonoBehaviour
     {
         if (_playerRB == null) return;
 
-        if (_isGrounded)
+        if (_canJump)
             _playerRB.AddForceY(jumpForce, ForceMode2D.Impulse);
     }
 
@@ -68,6 +72,10 @@ public class PlayerController : MonoBehaviour
     void GroundCheck()
     {
         _isGrounded = Physics2D.Raycast((Vector2)transform.position + startPointOffSet, Vector2.right, groundCheckDistance, groundLayer);
+        if (_isGrounded)
+        {
+            _lastGroundedCheck = Time.time;
+        }
     }
 
     void OnDrawGizmos()
